a11f63d4152d0ca4b0cb35e49441129f852a9737
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2019-03-15 17:13:10 -0300

    Importando estrutura inicial do projeto

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CardPokestopComponent } from './card-pokestop/card-pokestop.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CardPokestopComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/card-pokestop/card-pokestop.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pokestop',
+  templateUrl: './card-pokestop.component.html'
+})
+export class CardPokestopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
