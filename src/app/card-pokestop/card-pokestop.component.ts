0a153fc4256c814fed38914f98bd45c7aa59861e
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2020-07-16 19:50:38 -0300

    Exemplo de @Input() e @Output()

--- src/app/card-pokestop/card-pokestop.component.ts
+++ src/app/card-pokestop/card-pokestop.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-card-pokestop',
@@ -6,10 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardPokestopComponent implements OnInit {
 
+  _info: any;
+
+
+  @Output() novoItem = new EventEmitter<any>();
+
+  newCard = {
+    img: './assets/images/pokestops/morumbi/spiral-sculp.jpg',
+    name: 'Spiral Sculp',
+    location: `https://www.google.com.br/maps/place/23%C2%B037'36.2%22S+46%C2%B042'05.6%22W/@-23.6267258,-46.7020922,19z/data=!3m1!4b1!4m6!3m5!1s0x0:0x0!7e2!8m2!3d-23.6267268!4d-46.7015445`,
+    address: 'Rua Enxovia, 472 - Chacará Santo Antônio - SP',
+    questName: 'Evoluir um pokemon usando 1 item',
+    reward: 'Eevee (400 - 455)',
+  };
+
+  @Input()
+  set info(value) {
+    this._info = value;
+  }
+
+  @Output() titulo = new EventEmitter();
+  @Output() novoCard = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  onClick(titulo: any) {
+    this.titulo.emit(titulo);
+    this.novoCard.emit(this.newCard);
+  }
+
+  sendMessage() {
+    of([]).subscribe(
+      res => {
+        this.novoItem.emit(res);
+      },
+      err => {}
+    )
+  }
+
+  _onNovoEvento(event) {
+
+  }
+
 
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { of } from 'rxjs';
+import { CardPokestopComponent } from '../card-pokestop/card-pokestop.component';
 
 @Component({
   selector: 'app-home',
@@ -6,9 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  tituloCard = 'Teste';
+  pokestops = [];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  @ViewChild(CardPokestopComponent) card: CardPokestopComponent;
+
+  getComunicados() {
+    of([]).subscribe(
+      res => {
+        this.pokestops = res;
+      },
+      err => {
+        alert('deu erro')
+      }
+    )
+  }
+
+  getCards() {
+    this.pokestops = [
+      {
+        img: './assets/images/pokestops/morumbi/spiral-sculp.jpg',
+        name: 'Spiral Sculp',
+        location: `https://www.google.com.br/maps/place/23%C2%B037'36.2%22S+46%C2%B042'05.6%22W/@-23.6267258,-46.7020922,19z/data=!3m1!4b1!4m6!3m5!1s0x0:0x0!7e2!8m2!3d-23.6267268!4d-46.7015445`,
+        address: 'Rua Enxovia, 472 - Chacará Santo Antônio - SP',
+        questName: 'Evoluir um pokemon usando 1 item',
+        reward: 'Eevee (400 - 455)',
+      },
+      {
+        img: './assets/images/pokestops/morumbi/azulejos-randa.jpg',
+        name: 'Iguatemi Shopping',
+        location: `https://www.google.com.br/maps/place/23%C2%B037'36.2%22S+46%C2%B042'05.6%22W/@-23.6267258,-46.7020922,19z/data=!3m1!4b1!4m6!3m5!1s0x0:0x0!7e2!8m2!3d-23.6267268!4d-46.7015445`,
+        address: 'Rua Enxovia, 472 - Chacará Santo Antônio - SP',
+        questName: 'Evoluir um pokemon usando 1 item',
+        reward: 'Eevee (400 - 455)',
+      },
+    ]
+  }
+
+  _onTitulo(event: any) {
+    this.tituloCard = event;
+  }
+
+  _novoCard(event: any) {
+    this.pokestops.push(event);
   }
 
 }
